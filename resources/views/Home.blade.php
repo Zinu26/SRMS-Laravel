adb844e90a873dab2efb42c0d81998b0903c7131
Rhoss Gerald Bisda <rhoss123bisda@gmail.com>
2023-08-08 21:05:12 +0800

    initial commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="container-box">
+        <div>
+            <label class="control-label">Username</label>
+            <input type="text" name="Username" placeholder="Username">
+        </div>
+        <div>
+            <label class="control-label">Password</label>
+            <input type="password" name="Username" placeholder="Password">
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </div>
+</div>
